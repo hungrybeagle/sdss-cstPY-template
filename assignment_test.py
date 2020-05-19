feaa69823e7afc5da17e78720a0d720aa0b1664d
Chris Yang <cyang@deltalearns.ca>
2020-05-19 14:42:08 -0700

    Create assignment_test.py

---
+++ assignment_test.py
@@ -0,0 +1,12 @@
+#! python3
+
+import assignment
+
+def test1():
+  assert assignment.function() == expected output
+
+def test2():
+  assert assignment.function() == expected output
+
+def test3():
+  assert assignment.function() == expected output
