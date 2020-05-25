def5515dc9bd0bf7e2c9cde945f3434be5bbf58c
Chris Yang <cyang@deltalearns.ca>
2020-05-25 12:53:28 -0700

    Update assignment_test.py

--- assignment_test.py
+++ assignment_test.py
@@ -6,7 +6,8 @@ def test1():
   assert assignment.function() == expected output
 
 def test2():
-  assert assignment.function() == expected output
+  assert "needle" in assignment.function()
+  assert "needle2" in assignment.function(),"error Message"
 
 def test3():
   assert assignment.function() == expected output
